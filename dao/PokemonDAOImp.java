32d739896319099208b256d339d88a4ba23d9759
Gabo <Gabo>
2021-09-30 14:00:06 +0200

    Fix PokemonDAOImp

--- dao/PokemonDAOImp.java
+++ dao/PokemonDAOImp.java
@@ -7,61 +7,61 @@ import java.util.List;
 import modelo.Alumno;
 import modelo.Pokemon;
 
-public class PokemonDAOImp implements AulaDAO {
+public class PokemonDAOImp implements PokemonDAO {
 
 	private List<Pokemon> pokemones;
 	private int numPokemones; // atributo para controlar el n�mero real de
-	// elementos que tiene nuestro almac�n
-
-	/**
-	 * Constructor del Almac�n con un determinado tamano
-	 * 
-	 * @param tamano
-	 */
-	public PokemonDAOImp(int tamano) {
-		pokemones = new ArrayList<Pokemon>(tamano);
-		numPokemones = tamano;
-	}
 
 	@Override
 	public boolean estaVacio() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean estaLLeno() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public void add(Alumno alumno) {
-		// TODO Auto-generated method stub
+	public void add(Pokemon pokemon) {
 
 	}
 
 	@Override
-	public boolean eliminar(Alumno alumno) {
-		// TODO Auto-generated method stub
+	public boolean eliminar(Pokemon pokemon) {
 		return false;
 	}
 
 	@Override
-	public void informacionAlumnos() {
-		// TODO Auto-generated method stub
+	public void escribirPokemon(String ruta, String name, int life, int atack, int defense, int specialAttack, int specialdefense, int speed) {
 
 	}
 
 	@Override
-	public void escribeAlumnos(Path ruta) {
-		// TODO Auto-generated method stub
+	public void imprimirPokemon(String ruta) {
 
 	}
 
 	@Override
-	public void leeAlumnos(Path ruta) {
-		// TODO Auto-generated method stub
+	public void escribirPokemon(String ruta, Pokemon pokemon) {
 
 	}
+
+	@Override
+	public List<Pokemon> leerPokemon(String ruta) {
+		return null;
+	}
+
+	@Override
+	public List<Pokemon> leerPokemon(String ruta, String nombre) {
+		return null;
+	}
+
+	@Override
+	public void escribirFlotante(float numeroDecimal, String ruta) {
+
+	}
+	// elementos que tiene nuestro almac�n
+
+
 }
