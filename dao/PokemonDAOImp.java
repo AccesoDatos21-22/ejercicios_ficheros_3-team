9b8ee943f929334d21e7d2b0984a67e79d1ff598
Gabo <Gabo>
2021-10-04 10:19:09 +0200

    1 y 2

--- dao/PokemonDAOImp.java
+++ dao/PokemonDAOImp.java
@@ -1,5 +1,6 @@
 package dao;
 
+import java.io.*;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
@@ -34,13 +35,50 @@ public class PokemonDAOImp implements PokemonDAO {
 
 	@Override
 	public void escribirPokemon(String ruta, String name, int life, int atack, int defense, int specialAttack, int specialdefense, int speed) {
+		try {
+			BufferedWriter bwPok = new BufferedWriter(new FileWriter(ruta,true));
+			Pokemon pok = new Pokemon(name,life,atack,defense,specialAttack,specialdefense,speed);
+			if (!pok.existe(ruta)){
+				bwPok.write(pok.toString()+"\n");
+			}else{
+				System.out.println(name+" ya existe.");
+			}
+			bwPok.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
 	@Override
-	public void imprimirPokemon(String ruta) {
+	public void imprimirPokemon (String ruta) {
 
+		try {
+			BufferedReader brPoke = new BufferedReader(new FileReader(ruta));
+			String linea;
+			String pokeS[];
+			while ((linea = brPoke.readLine()) != null) {
+				pokeS = linea.split(";");
+				Pokemon pok = new Pokemon(pokeS[0],Integer.parseInt(pokeS[1]),Integer.parseInt(pokeS[2]),Integer.parseInt(pokeS[3]),Integer.parseInt(pokeS[4]),Integer.parseInt(pokeS[5]),Integer.parseInt(pokeS[6]));
+				pok.toString();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		/*System.out.println(
+				"nombre='" + nombre + '\'' +
+						", vida=" + vida +
+						", ataque=" + ataque +
+						", defensa=" + defensa +
+						", ataqueEspecial=" + ataqueEspecial +
+						", defensaEspecial=" + defensaEspecial +
+						", velosidad=" + velosidad
+		);*/
 	}
+
 
 	@Override
 	public void escribirPokemon(String ruta, Pokemon pokemon) {
--- modelo/Pokemon.java
+++ modelo/Pokemon.java
@@ -1,5 +1,96 @@
 package modelo;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Objects;
+
 public class Pokemon {
+
+    private String nombre;
+    private int vida;
+    private int ataque;
+    private int defensa;
+    private int ataqueEspecial;
+    private int defensaEspecial;
+    private int velosidad;
+
+    public Pokemon(String nombre, int vida, int ataque, int defensa, int ataqueEspecial, int defensaEspecial, int velosidad) {
+        this.nombre = nombre;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defensa = defensa;
+        this.ataqueEspecial = ataqueEspecial;
+        this.defensaEspecial = defensaEspecial;
+        this.velosidad = velosidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public int getAtaqueEspecial() {
+        return ataqueEspecial;
+    }
+
+    public int getDefensaEspecial() {
+        return defensaEspecial;
+    }
+
+    public int getVelosidad() {
+        return velosidad;
+    }
+
+    public boolean existe(String ruta ) {
+        boolean ex= false;
+        try {
+            BufferedReader brPoke = new BufferedReader(new FileReader(ruta));
+            String linea;
+            while ((linea = brPoke.readLine()) != null) {
+                if(this.getNombre().equals(linea.split(";")[0])) {
+                    ex=true;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ex;
+    }
+
+    @Override
+    public String toString() {
+        return
+                nombre + ';' +
+                vida + ';' +
+                ataque + ';' +
+                defensa + ';' +
+                ataqueEspecial + ';' +
+                defensaEspecial + ';' +
+                velosidad
+                ;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pokemon pokemon = (Pokemon) o;
+        return Objects.equals(nombre, pokemon.nombre);
+    }
 
 }
