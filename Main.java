bee99b8a082ff6bc7f40b253e2358e9f44b9eefc
Gabo <Gabo>
2021-10-07 12:29:02 +0200

    Ejercicios hasta el 6

--- Main.java
+++ Main.java
@@ -1,11 +1,27 @@
+import ficheros.Ejercicios1_3;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		
-		
 
+		Ejercicios1_3 ej = new Ejercicios1_3();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("¿Cual es el nombre del fichero?");
+		Path pa = ej.getNombre(sc);
+
+		List<String> li = ej.getFrases(sc);
+
+		ej.escribefrases(li,pa);
+
+		sc.close();
 	}
 
 }
--- ficheros/Ejercicios1_3.java
+++ ficheros/Ejercicios1_3.java
@@ -1,7 +1,13 @@
 package ficheros;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 import interfaces.InterfazEjercicios1_3;
@@ -20,20 +26,46 @@ public class Ejercicios1_3 implements InterfazEjercicios1_3 {
 	
 	@Override
 	public List<String> getFrases(Scanner escaner) {
-		
-		return null;
+
+		String frasecita = "";
+		List<String> frases = new ArrayList<String>();
+
+		while (!(frasecita=escaner.nextLine()).equals("salir")) {
+			frases.add(frasecita);
+		}
+		return frases;
 	}
 
 	@Override
 	public Path getNombre(Scanner escaner) {
-		
-		return null;
+		Path pa = null;
+		try {
+			pa = Paths.get(escaner.nextLine());
+
+			if (!Files.exists(pa)) {
+				System.out.println("El fichero no existe en el directorio actual");
+			}
+
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return pa;
 	}
 	
 	@Override
-	public void escribefrases(List<String> cadenas, Path ruta) {
-		
-		
+	public void escribefrases(Path ruta) {
+
+		try {
+
+
+
+			BufferedWriter bw= new BufferedWriter(new FileWriter(ruta.toString()));
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
