97abc7abd4c6c67f55caa86c48069ebfc53b7487
Gabo <Gabo>
2021-11-09 10:54:59 +0100

    Porfón

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
+import dao.AulaDAOImp;
 import dao.PokemonDAO;
 import dao.PokemonDAOImp;
+import modelo.Alumno;
 import modelo.Pokemon;
 import ficheros.Ejercicios1_3;
 
@@ -7,6 +9,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.List;
 import java.util.Scanner;
@@ -49,9 +52,15 @@ public class Main {
 		PokemonDAOImp poli = new PokemonDAOImp();
 		poli.add(pokimon);
 		poli.escribirPokemon("ob.bin",pokimon);
-		List<Pokemon> list = poli.leerPokemon("ob.bin");
-		System.out.println(list);
+//		List<Pokemon> list = poli.leerPokemon("ob.bin");
+//		System.out.println(list);
 
+		//Pruebas aula
+		AulaDAOImp aula = new AulaDAOImp(2);
+		aula.add(new Alumno("Paco","Hernandez Paine",500,"Calle del Melon",69));
+		aula.add(new Alumno("Pedro","Hernandez Paine",500,"Calle del Melon",69));
+		aula.escribeAlumnos(Paths.get("Alum.txt"));
+		aula.leeAlumnos(Paths.get("Alum.txt"));
 	}
 
 }
